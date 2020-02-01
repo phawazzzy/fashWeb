2f4cee01dc872e74c2102c1061e6cdb34293a0ea
kareem fawas <phawazzzy@gmail.com>
2020-02-01 12:48:22 +0100

    corrected the orderSchema to productSchema

---
+++ models/products.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    productImage: { type: String },
+    productName: { type: String },
+    price: { type: Number },
+    tag: { type: String }
+})
+
+mongoose.model('product', productSchema);
