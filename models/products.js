0d0c0994635e6eba495d53be7f515fd235e7e3a7
kareem fawas <phawazzzy@gmail.com>
2020-02-01 12:52:26 +0100

    exported the module

--- models/products.js
+++ models/products.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
     productImage: { type: String },
+    productPublic_id: { type: String},
     productName: { type: String },
     price: { type: Number },
     tag: { type: String }
 })
 
-mongoose.model('product', productSchema);
+module.exports = mongoose.model('product', productSchema);
